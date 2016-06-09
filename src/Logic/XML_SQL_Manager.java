7145688723c25ca1f178cc8457dd1744fb1a87f4
Tomek <ghost744@gmail.com>
2016-06-09 23:29:30 +0200

    omg

---
+++ src/Logic/XML_SQL_Manager.java
@@ -0,0 +1,5 @@
+package Logic;
+
+public class XML_SQL_Manager {
+
+}
---
+++ src/tgrkKompo/source.java
@@ -0,0 +1,12 @@
+package tgrkKompo;
+
+import javax.swing.JApplet;
+
+public class source extends JApplet{
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
